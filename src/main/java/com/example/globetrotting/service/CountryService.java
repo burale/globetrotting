1be38224bf6154a5ecac95eded8ff98e174f8b7e
Liibaan <burale2000@hotmail.com>
2019-10-29 20:23:45 +0000

    Circle Added

--- src/main/java/com/example/globetrotting/service/CountryService.java
+++ src/main/java/com/example/globetrotting/service/CountryService.java
@@ -57,7 +57,7 @@ public class CountryService {
         if (reachableCapitalCities.size()>0){
             Collections.sort(reachableCapitalCities, new Country());
         }
-        
+
 
 
         return reachableCapitalCities;
